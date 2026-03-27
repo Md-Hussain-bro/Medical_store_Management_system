1b52313f6835bdd543efd84747f133f98f3ded97
Hussain Ansari <hussainansari8995@gmail.com>
2026-03-28 00:03:02 +0530

    add medicine

--- app.py
+++ app.py
@@ -38,7 +38,13 @@ app = Flask(
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'dev-secret-key'
 os.makedirs(app.instance_path, exist_ok=True)
 
-db_path = os.path.join(app.instance_path, "pharmacy.db")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+instance_folder = os.path.join(BASE_DIR, "instance")
+os.makedirs(instance_folder, exist_ok=True)
+
+db_path = os.path.join(instance_folder, "pharmacy.db")
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
